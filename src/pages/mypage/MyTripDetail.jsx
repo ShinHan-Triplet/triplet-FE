8b019429a1a4312cbaa8bcea5db9f806e67c099c
Joo Won <110383412+chqkq@users.noreply.github.com>
2025-09-26 18:02:11 +0900

    WIP: pull받기 용

--- src/pages/mypage/MyTripDetail.jsx
+++ src/pages/mypage/MyTripDetail.jsx
@@ -286,7 +286,7 @@ export default function MyTripDetail() {
               textColor={colors.white}
               width={160}
               hoverBgColor={colors.blue500}
-              onClick={() => {}}
+              onClick={() => navigate(`/mypage/trip/${view?.id}/edit`)}
             />
           )}
         </BtnRow>
